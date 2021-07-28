6d4e526861cfbfe34d3ac5a5f8037db0fc771d85
Digdemk <digdemkonak@gmail.com>
2021-07-28 14:02:55 +0300

    last

---
+++ BilgeAdam.Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BilgeAdam.Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
